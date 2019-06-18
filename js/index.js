75b784456923366aa69ce3b91482483125227ca5
Kalyn Williams <40311343+KalynWilliams@users.noreply.github.com>
2019-06-18 15:17:43 -0700

    added wheel and drag and drop!

--- js/index.js
+++ js/index.js
@@ -15,3 +15,29 @@ document.addEventListener('keydown', (event) => {
     body.style.backgroundColor = '#FFEBCD';
 
  });
+
+ //wheel
+ // increase header text on scroll over
+ const introHeader = document.querySelector('.intro h2');
+ introHeader.addEventListener('wheel', (event)=> {
+     event.preventDefault();
+     event.currentTarget.style.fontSize = '10rem';
+ });
+// drag and drop
+ const introImg = document.querySelector('.intro img');
+ const goImg = document.querySelector('.img-content img');
+
+
+  introImg.addEventListener('drag',(event) => {
+     event.currentTarget.style.width = '400px';
+ })
+ introImg.addEventListener('dragend',(event) => {
+     event.currentTarget.style.width = '800px';
+ })
+ introHeader.addEventListener('dragover',(event) => {
+     event.preventDefault();
+ });
+ introHeader.addEventListener('drop',(event) => {
+     body.style.backgroundColor = 'pink';
+     body.style.color = 'white';
+ });
