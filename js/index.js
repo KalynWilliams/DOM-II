f949e72de67ab9588c6a8560af3008fb01a928d1
Kalyn Williams <40311343+KalynWilliams@users.noreply.github.com>
2019-06-18 14:47:48 -0700

    Added mouseover and keydown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// mouseover
+// set anchor text color on mouseover
+
+const anchors = Array.from(document.querySelectorAll('.nav a'));
+anchors.forEach(e => e.addEventListener('mouseover',(event) => {
+    event.currentTarget.style.color = '#17a2b8';
+}));
+
+//keydown
+//change body color on keypress
+const body = document.querySelector('header');
+document.addEventListener('keydown', (event) => {
+    body.style.backgroundColor = '#FFEBCD';
+
+ });
